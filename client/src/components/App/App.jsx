753f989a1f5af3a2034d3afbb0ee0e1e5e589dd6
Pauolosavery <pauolosavery1@gmail.com>
2022-08-24 15:18:23 +0300

    start project client

---
+++ client/src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import { useEffect } from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+export default function App() {
+  return (
+
+        <div className="App">
+
+        </div>
+  );
+}
